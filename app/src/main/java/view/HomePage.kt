a847394d60f9b845ad9608cd9b975a810dc8ced8
rnktlhr <rinakitluhur10@gmail.com>
2025-11-07 12:42:43 +0700

    memambahkan colom di home page

--- app/src/main/java/view/HomePage.kt
+++ app/src/main/java/view/HomePage.kt
@@ -1,6 +1,13 @@
 package com.example.tugas4.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WelcomeScreen(
@@ -8,5 +15,11 @@ fun WelcomeScreen(
     nim: String = "20230140069",
     onSubmitClick: () -> Unit
 ) {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp, vertical = 48.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {}
 }
