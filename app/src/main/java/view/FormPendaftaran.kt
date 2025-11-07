6af58953183002dc0e8a408b609920d705a5b51d
rnktlhr <rinakitluhur10@gmail.com>
2025-11-07 12:57:41 +0700

    menambahkan val pada formulir pendaftaran

--- app/src/main/java/view/FormPendaftaran.kt
+++ app/src/main/java/view/FormPendaftaran.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.input.TextFieldValue
 
 @Composable
@@ -13,4 +14,9 @@ fun FormulirPendaftaran(modifier: Modifier){
     val alamat by remember { mutableStateOf(TextFieldValue("")) }
     val jenisKelamin by remember { mutableStateOf("") }
     val statusPerkawinan by remember { mutableStateOf("") }
+
+    val paddingSmall = dimensionResource(id = R.dimen.padding_small)
+    val paddingMedium = dimensionResource(id = R.dimen.padding_medium)
+    val paddingLarge = dimensionResource(id = R.dimen.padding_large)
+    val buttonHeight = dimensionResource(id = R.dimen.button_height)
 }
