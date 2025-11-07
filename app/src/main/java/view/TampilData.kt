e31d15fa14f996f8ab4d31c5d901cea389ce28b1
rnktlhr <rinakitluhur10@gmail.com>
2025-11-07 12:32:15 +0700

    menambahkan isi ruang colom text dan HorizontalDivider di tampildata

--- app/src/main/java/view/TampilData.kt
+++ app/src/main/java/view/TampilData.kt
@@ -1,6 +1,10 @@
 package com.example.tugas4.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -9,7 +13,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tugas4.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -34,5 +43,29 @@ fun TampilData(
                     .mediumTopAppBarColors(colorResource(id=R.color.teal_700)
                     )
             )
-        }){}
+        }){
+            isiRuang->
+            Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween) {
+            Column(
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+            ) {
+                items.forEach { item ->
+                    Column {
+                        Text(
+                            text = item.first.uppercase(),
+                            fontSize = 16.sp
+                        )
+                        Text(
+                            text = item.second,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = FontFamily.Cursive, fontSize = 22.sp
+                        )
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
+            }
+        }
+    }
 }
