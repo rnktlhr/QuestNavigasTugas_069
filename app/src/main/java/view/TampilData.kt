61ea91ff41c9cc172ebce2d063d9e7d3a7bcb406
rnktlhr <rinakitluhur10@gmail.com>
2025-11-07 12:30:04 +0700

    menambahkan Scaffold, TopAppBar dan txt di tampildata

--- app/src/main/java/view/TampilData.kt
+++ app/src/main/java/view/TampilData.kt
@@ -1,8 +1,16 @@
 package com.example.tugas4.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import com.example.tugas4.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -14,4 +22,17 @@ fun TampilData(
         Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
         Pair(stringResource(id = R.string.alamat), "Yogyakarta")
     )
+    Scaffold(modifier = Modifier,
+        {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.tampil),
+                        color = Color.White
+                    )},
+                colors = TopAppBarDefaults
+                    .mediumTopAppBarColors(colorResource(id=R.color.teal_700)
+                    )
+            )
+        }){}
 }
