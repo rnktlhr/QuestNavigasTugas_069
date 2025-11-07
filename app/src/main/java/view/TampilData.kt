bee20520faa097297f6d3bbefd8e7b43232e462e
rnktlhr <rinakitluhur10@gmail.com>
2025-11-07 12:28:39 +0700

    menambahkan val di tampildata

--- app/src/main/java/view/TampilData.kt
+++ app/src/main/java/view/TampilData.kt
@@ -2,9 +2,16 @@ package com.example.tugas4.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     onBackBtnClick:()->Unit
-){}
+) {
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
+        Pair(stringResource(id = R.string.alamat), "Yogyakarta")
+    )
+}
