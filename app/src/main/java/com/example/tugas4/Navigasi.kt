f2ff3f6208e462491d1619c7037d4b5be931587f
rnktlhr <rinakitluhur10@gmail.com>
2025-11-07 13:09:18 +0700

    menambahkan composable dan fun dataapp di navigasi

--- app/src/main/java/com/example/tugas4/Navigasi.kt
+++ app/src/main/java/com/example/tugas4/Navigasi.kt
@@ -1,7 +1,20 @@
 package com.example.tugas4
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi {
     Home,
     FormPendaftaran,
     Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+) {
+
 }
