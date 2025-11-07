a41b7693e2772863c080f50ff4ed7abb2cd3c8f0
rnktlhr <rinakitluhur10@gmail.com>
2025-11-07 12:41:35 +0700

    memambahkan composable dan fun welcome screen di home page

---
+++ app/src/main/java/view/HomePage.kt
@@ -0,0 +1,12 @@
+package com.example.tugas4.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WelcomeScreen(
+    nama: String = "Rinakit Luhur",
+    nim: String = "20230140069",
+    onSubmitClick: () -> Unit
+) {
+
+}
