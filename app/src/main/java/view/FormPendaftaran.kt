abd1ef49d17bdfa8e8776491c2436b9797697fb1
rnktlhr <rinakitluhur10@gmail.com>
2025-11-07 12:53:50 +0700

    menambahkan val pada formulir pendaftaran

--- app/src/main/java/view/FormPendaftaran.kt
+++ app/src/main/java/view/FormPendaftaran.kt
@@ -1,7 +1,16 @@
 package com.example.tugas4
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 
 @Composable
-fun FormulirPendaftaran(modifier: Modifier){}
+fun FormulirPendaftaran(modifier: Modifier){
+    val nama by remember { mutableStateOf(TextFieldValue("")) }
+    val alamat by remember { mutableStateOf(TextFieldValue("")) }
+    val jenisKelamin by remember { mutableStateOf("") }
+    val statusPerkawinan by remember { mutableStateOf("") }
+}
