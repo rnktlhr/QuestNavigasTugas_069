d48d1fabb03929bb79d9d051f7a9969aa6268739
rnktlhr <rinakitluhur10@gmail.com>
2025-11-07 12:43:35 +0700

    memambahkan text di dalam colom yang ada di home page

--- app/src/main/java/view/HomePage.kt
+++ app/src/main/java/view/HomePage.kt
@@ -4,10 +4,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun WelcomeScreen(
@@ -21,5 +26,13 @@ fun WelcomeScreen(
             .padding(horizontal = 24.dp, vertical = 48.dp),
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {}
+    ) {
+        Text(
+            text = "Selamat Datang",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF3A3A9E),
+            textAlign = TextAlign.Center
+        )
+    }
 }
