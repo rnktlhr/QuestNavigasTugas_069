0771244a009ad0b0f02737bfe9f6d086e34a3808
rnktlhr <rinakitluhur10@gmail.com>
2025-11-07 13:03:58 +0700

    menambahkan colum text dan OutlinedTextField di form pendaftaran

--- app/src/main/java/view/FormPendaftaran.kt
+++ app/src/main/java/view/FormPendaftaran.kt
@@ -13,12 +13,14 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,7 +33,7 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun FormulirPendaftaran(modifier: Modifier){
-    val nama by remember { mutableStateOf(TextFieldValue("")) }
+    var nama by remember { mutableStateOf(TextFieldValue("")) }
     val alamat by remember { mutableStateOf(TextFieldValue("")) }
     val jenisKelamin by remember { mutableStateOf("") }
     val statusPerkawinan by remember { mutableStateOf("") }
@@ -78,7 +80,21 @@ fun FormulirPendaftaran(modifier: Modifier){
                 colors = CardDefaults.cardColors(containerColor = Color.White),
                 elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
             ) {
-
+                Column(
+                    modifier = Modifier
+                        .padding(paddingMedium)
+                        .fillMaxWidth()
+                ) {
+                    Text("NAMA LENGKAP", fontWeight = FontWeight.Bold)
+                    OutlinedTextField(
+                        value = nama,
+                        onValueChange = { nama = it },
+                        placeholder = { Text("Isian nama lengkap") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = paddingSmall)
+                    )
+                }
             }
         }
     }
