310b465e090b7a8c7127663dae00021dd99f563e
rnktlhr <rinakitluhur10@gmail.com>
2025-11-07 11:29:55 +0700

    menambahkan Scaffold yang ada topappbar

--- app/src/main/java/view/Formulir.kt
+++ app/src/main/java/view/Formulir.kt
@@ -2,10 +2,31 @@
 package com.example.tugas4
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun FormIsian(
     jenisK: List<String> = listOf("Laki-Laki", "Perempuan"),
     onSubmitBtnClick : () -> Unit
-){}
+){
+    Scaffold(modifier = Modifier,
+        {
+            TopAppBar(
+                title = {Text(stringResource(id= R.string.home),
+                    color = Color.White)},
+                colors = TopAppBarDefaults.topAppBarColors
+                    (colorResource(id=R.color.teal_700))
+            )}
+
+    ){
+
+    }
+}
