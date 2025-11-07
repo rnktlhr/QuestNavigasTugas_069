fa9d1e007550899cd6e6c6c453681e8ce76cd3bb
rnktlhr <rinakitluhur10@gmail.com>
2025-11-07 12:59:00 +0700

    menambahkan surface dan colum

--- app/src/main/java/view/FormPendaftaran.kt
+++ app/src/main/java/view/FormPendaftaran.kt
@@ -1,12 +1,19 @@
 package com.example.tugas4
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormulirPendaftaran(modifier: Modifier){
@@ -19,4 +26,17 @@ fun FormulirPendaftaran(modifier: Modifier){
     val paddingMedium = dimensionResource(id = R.dimen.padding_medium)
     val paddingLarge = dimensionResource(id = R.dimen.padding_large)
     val buttonHeight = dimensionResource(id = R.dimen.button_height)
+
+    Surface(
+        color = Color(0xFFF6ECFF),
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        }
+    }
 }
