fa8ac40d9ce1f2c8f0627538a51ca7ef2f47a772
rnktlhr <rinakitluhur10@gmail.com>
2025-11-07 13:25:52 +0700

    menambahkan data app di main aktivity

--- app/src/main/java/com/example/tugas4/MainActivity.kt
+++ app/src/main/java/com/example/tugas4/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Tugas4Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
