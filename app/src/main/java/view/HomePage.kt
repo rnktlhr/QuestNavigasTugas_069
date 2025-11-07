37059269731018fc1c29f229d0d2d68d72b510b7
rnktlhr <rinakitluhur10@gmail.com>
2025-11-07 12:45:27 +0700

    memambahkan image di dalam colom yang ada di home page

--- app/src/main/java/view/HomePage.kt
+++ app/src/main/java/view/HomePage.kt
@@ -1,18 +1,22 @@
 package com.example.tugas4.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.tugas4.R
 
 @Composable
 fun WelcomeScreen(
@@ -34,5 +38,13 @@ fun WelcomeScreen(
             color = Color(0xFF3A3A9E),
             textAlign = TextAlign.Center
         )
+        Image(
+            painter = painterResource(id = R.drawable.ugm),
+            contentDescription = "",
+            modifier = Modifier
+                .size(200.dp)
+                .padding(vertical = 16.dp)
+        )
     }
+
 }
