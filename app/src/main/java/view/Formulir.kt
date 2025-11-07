73e37394757569b0c2e62a15d84ead71636268a4
rnktlhr <rinakitluhur10@gmail.com>
2025-11-07 11:27:24 +0700

    menambahkan composable dan fun formIsian yang terdiri atas jenis kelamin dan submit btn

--- app/src/main/java/view/Formulir.kt
+++ app/src/main/java/view/Formulir.kt
@@ -2,4 +2,10 @@
 package com.example.tugas4
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
 
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-Laki", "Perempuan"),
+    onSubmitBtnClick : () -> Unit
+){}
