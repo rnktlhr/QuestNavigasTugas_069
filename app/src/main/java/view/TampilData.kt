ef683fe410f2d355a3ea659342f5657a98d29451
rnktlhr <rinakitluhur10@gmail.com>
2025-11-07 12:27:48 +0700

    menambahkan @OptIn dan composebal serta fun TampilData di tampildata

--- app/src/main/java/view/TampilData.kt
+++ app/src/main/java/view/TampilData.kt
@@ -1,1 +1,10 @@
 package com.example.tugas4.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnClick:()->Unit
+){}
